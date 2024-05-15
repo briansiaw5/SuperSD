68ebd84038c5b5558f5adf8479dae636007d1261
Brian Siaw <briansiaw5@gmail.com>
2024-05-15 23:43:13 +0000

    clone

--- config.js
+++ config.js
@@ -10,5 +10,5 @@ module.exports = {
         googleMaps: 'your-google-maps-api-key',
         someOtherApi: 'your-other-api-key'
     },
-    environment: 'development'
+    environment: 'development clone'
 };
